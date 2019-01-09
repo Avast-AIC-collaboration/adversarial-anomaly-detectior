5be97cdb13f1136e777f9862d0fdc663ecf143fb
Karel Durkota <karel.durkota@gmail.com>
2019-01-09 14:16:20 +0100

    Implemented gaussian probability distribution.

--- data_featured/dataset_featured.py
+++ data_featured/dataset_featured.py
@@ -2,28 +2,27 @@ import pandas as pd
 import numpy as np
 
 
-class ProbabilityDistribution:
+class DatasetFeatures:
 
-    def __init__(self, data):
+    def __init__(self, data, features):
         self.data = data
+        self.features = features
 
     @classmethod
     def from_file(cls, file_name, force=False):
         pass
 
     @classmethod
-    def from_normal_distribution_independent(cls, labels=['F1', 'F2'], mean=[20, 10], var=[12, 4], size=1000, force=False):
+    def from_normal_distribution_independent(cls, features=['F1', 'F2'], mean=[20, 10], var=[12, 4], size=1000, force=False):
         data = pd.DataFrame()
 
-        for l, m, v in zip(labels, mean, var):
+        for l, m, v in zip(features, mean, var):
             data[l] = np.random.normal(m, v, size)
 
-        res = cls(data)
+        res = cls(data, features=features)
         return res
 
 
-def testThisClass():
-    c = ProbabilityDistribution.from_normal_distribution_independent()
-    print(c.data)
-
-testThisClass()
+# def testThisClass():
+#     c = DatasetFeatures.from_normal_distribution_independent()
+#     print(c.data)
--- probability_distribution/probability_distribution.py
+++ probability_distribution/probability_distribution.py
@@ -1,32 +1,35 @@
-import pandas as pd
+from scipy.stats import gaussian_kde
 import numpy as np
+import matplotlib.pyplot as plt
+from data_featured.dataset_featured import DatasetFeatures
 
 
 class ProbabilityDistribution:
-
-    def __init__(self, data):
-        self.data = data
-        self.cache = {}
-        self.total = len(data)
-
-    @classmethod
-    def from_file(cls, file_name, force=False):
-        pass
-
-    @classmethod
-    def from_normal_distribution_independent(cls, labels=['F1', 'F2'], mean=[20, 10], var=[12, 4], size=1000, force=False):
-        F = len(mean)
-        data = pd.DataFrame()
-
-        for l, m, v in zip(labels, mean, var):
-            data[l] = np.random.normal(m, v, size)
-
-        res = cls(data)
-        return res
+    def __init__(self, data_featured:DatasetFeatures):
+        self.data_featured = data_featured
+        self.prb = gaussian_kde(data_featured.data.T)
 
 
-def testThisClass():
-    c = CumulativeProbCached.from_normal_distribution_independent()
+    def plot(self):
+        if self.prb.d == 1:
+            xs = np.linspace(0,1,10)
+            self.prb.covariance_factor = lambda : .25
+            self.prb._compute_covariance()
+            plt.plot(xs, self.prb(xs))
+            plt.show()
 
-    print(c.data)
+        if self.prb.d == 2:
+            X, Y = np.mgrid[0:1:100j, 0:1:100j]
+            positions = np.vstack([X.ravel(), Y.ravel()])
+            Z = np.reshape(self.prb(positions).T, X.shape)
 
+            fig, ax = plt.subplots()
+            ax.imshow(np.rot90(Z), cmap=plt.cm.gist_earth_r,
+                extent=[0, 1, 0, 1])
+            ax.plot(self.data_featured.data[self.data_featured.features[0]], self.data_featured.data[self.data_featured.features[1]], 'k.', markersize=2)
+            ax.set_xlim([0, 1])
+            ax.set_ylim([0, 1])
+            plt.show()
+
+        else:
+            print('Cannot plot distribution for dim>2.')
