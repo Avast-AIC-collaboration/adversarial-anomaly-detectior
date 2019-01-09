c271d39e7f68644b2dc798d9d641296172f156c9
Karel Durkota <karel.durkota@gmail.com>
2019-01-09 11:10:25 +0100

    project files.
    Added .gitignore
    
    Added simple Gaussian feature generator.
    
    moved probability distribution
    
    Fixed imports.
    
    moved probability distribution
    
    Refactorization - renaming.
    
    Implemented gaussian probability distribution.
    
    Updated means to be withing [0,1].
    
    Added simple game solver using Gurobi License.
    
    Partially working simple example.
    
    Finished computation of simple game.
    
    Generalized for N features.
    
    Separating methods.
    
    update .gitignore file.

---
+++ data_featured/dataset_featured.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+
+class DatasetFeatures:
+
+    def __init__(self, data, features):
+        self.data = data
+        self.features = features
+
+    @classmethod
+    def from_file(cls, file_name, force=False):
+        pass
+
+    @classmethod
+    def from_normal_distribution_independent(cls, features=['F1', 'F2'], mean=[0.3, 0.5], var=[0.1, 0.2], size=1000, force=False):
+        data = pd.DataFrame()
+        np.random.seed(seed=42)
+
+        for l, m, v in zip(features, mean, var):
+            data[l] = np.random.normal(m, v, size)
+
+        res = cls(data, features=features)
+        return res
+
+
+# def testThisClass():
+#     c = DatasetFeatures.from_normal_distribution_independent()
+#     print(c.data)
---
+++ game/game.py
@@ -0,0 +1,78 @@
+import multidict
+from gurobipy import *
+
+class Game(object):
+    def __init__(self, actions, utils, mesh, dist, FPrate, discount):
+        self.actions = actions
+        self.utils = utils # function
+        self.mesh = mesh
+        self.dist = dist # function
+        self.FPrate = FPrate
+        self.discount = discount
+
+    def solve(self):
+
+        try:
+            # Create variables
+            m = Model("game")
+            theta = m.addVars(len(self.actions), vtype=GRB.CONTINUOUS, ub=1.0, name="theta")
+            u = m.addVar(vtype=GRB.CONTINUOUS, name="obj")
+
+            # Set objective
+            m.setObjective(u, GRB.MINIMIZE)
+
+            # Add ctr: forall a: theta_a * r_a <= Ua
+            m.addConstrs(
+                ((1-theta[a]) * self.utils(self.mesh[a])/(1-self.discount) <= u for a in self.actions), "BRs")
+            print('Best response constraints done.')
+
+            m.addConstr(
+                quicksum(theta[a] * self.dist(self.mesh[a]) for a in self.actions) <= self.FPrate, "False-positive-rate")
+
+
+            m.write('./model.lp')
+
+            m.optimize()
+
+            self.thetas = [theta[i].X for i in theta]
+            # for v in m.getVars():
+            #     print(v.varName, v.x)
+
+            print('Obj:', m.objVal)
+            return m.objVal
+
+        except GurobiError as e:
+            print(str(e))
+            print('Error reported')
+
+
+class UtilityFunctions:
+    @staticmethod
+    def utility1(m):
+        return m[0]
+
+    def utilityPlus(m):
+        # print(m)
+        return m[0] + m[1]
+
+    def utilityMul(m):
+        # print(m)
+        return m[0] * m[1]
+
+
+def testThisClass():
+    actions = [0,1,2,3]
+    def dist(x):
+        r = [.4, .3, .2, .1]
+        return r[x]
+
+    def util(x):
+        return x
+
+    FPrate = 0.1
+    discount = 0.0
+    #
+    g = Game(actions, util, dist, FPrate, discount)
+    g.solve()
+
+# testThisClass()
