bd5f6a23d0ec037c9eade2437cc285f9fab7ed28
Karel Durkota <karel.durkota@gmail.com>
2019-01-09 11:16:03 +0100

    Added simple Gaussian feature generator.

---
+++ data_featured/DatasetFeatured.py
@@ -0,0 +1,34 @@
+import os
+import pickle
+import pandas as pd
+import numpy as np
+
+
+class CumulativeProbCached:
+
+    def __init__(self, data):
+        self.data = data
+        self.cache = {}
+        self.total = len(data)
+
+    @classmethod
+    def from_file(cls, file_name, force=False):
+        pass
+
+    @classmethod
+    def from_normal_distribution_independent(cls, labels=['F1', 'F2'], mean=[20, 10], var=[12, 4], size=1000, force=False):
+        F = len(mean)
+        data = pd.DataFrame()
+
+        for l, m, v in zip(labels, mean, var):
+            data[l] = np.random.normal(m, v, size)
+
+        res = cls(data)
+        return res
+
+
+def testThisClass():
+    c = CumulativeProbCached.from_normal_distribution_independent()
+
+    print(c.data)
+
