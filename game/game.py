403de531a6fffd8ea10fd40579c4e09fbe9de6e1
Karel Durkota <karel.durkota@gmail.com>
2019-01-10 15:57:59 +0100

    Generalized for N features.

--- game/game.py
+++ game/game.py
@@ -2,23 +2,20 @@ import multidict
 from gurobipy import *
 
 class Game(object):
-    def __init__(self, actions, utils, dist, FPrate, discount):
+    def __init__(self, actions, utils, mesh, dist, FPrate, discount):
         self.actions = actions
-        self.utils = utils
-        self.dist = dist
+        self.utils = utils # function
+        self.mesh = mesh
+        self.dist = dist # function
         self.FPrate = FPrate
         self.discount = discount
 
     def solve(self):
-        print(self.actions)
-        print(self.utils)
-        print(self.dist)
 
         try:
             # Create variables
             m = Model("game")
             theta = m.addVars(len(self.actions), vtype=GRB.CONTINUOUS, ub=1.0, name="theta")
-            print(theta)
             u = m.addVar(vtype=GRB.CONTINUOUS, name="obj")
 
             # Set objective
@@ -26,19 +23,18 @@ class Game(object):
 
             # Add ctr: forall a: theta_a * r_a <= Ua
             m.addConstrs(
-                ((1-theta[a]) * self.utils[a]/(1-self.discount) <= u for a in self.actions), "BRs")
+                ((1-theta[a]) * self.utils(self.mesh[a])/(1-self.discount) <= u for a in self.actions), "BRs")
             print('Best response constraints done.')
 
             m.addConstr(
-                quicksum(theta[i] * self.dist[i] for i in self.actions) <= self.FPrate, "False-positive-rate")
+                quicksum(theta[a] * self.dist(self.mesh[a]) for a in self.actions) <= self.FPrate, "False-positive-rate")
 
 
-            # m.write('./model.lp')
+            m.write('./model.lp')
 
             m.optimize()
 
             self.thetas = [theta[i].X for i in theta]
-            print(self.thetas)
             # for v in m.getVars():
             #     print(v.varName, v.x)
 
@@ -48,13 +44,25 @@ class Game(object):
             print(str(e))
             print('Error reported')
 
+
+class UtilityFunctions:
+    @staticmethod
+    def utility1(m):
+        return m[0]
+
+
 def testThisClass():
     actions = [0,1,2,3]
-    dist = [.4, .3, .2, .1]
+    def dist(x):
+        r = [.4, .3, .2, .1]
+        return r[x]
+
+    def util(x):
+        return x
+
     FPrate = 0.1
     discount = 0.0
     #
-    util = [i for i in actions]
     g = Game(actions, util, dist, FPrate, discount)
     g.solve()
 
--- runners/simple_experiment.py
+++ runners/simple_experiment.py
@@ -4,91 +4,77 @@ import matplotlib.pyplot as plt
 from math import log
 from data_featured.dataset_featured import DatasetFeatures
 from probability_distribution.probability_distribution import ProbabilityDistribution
-from game.game import Game
+from game.game import Game, UtilityFunctions
 
+# DEFINE CONSTANTS
+discretize=50
+FPrate = 0.1
+discount = 0
 
+fig, ax = plt.subplots(2,3)
+
+# get data
 data = DatasetFeatures.from_normal_distribution_independent()
 
-# print(data.data)
-
-prb = ProbabilityDistribution(data)
-# prb.prb.
-
-
-fig, ax = plt.subplots(2,2)
-
-# distcretization =
-
-
+# turn data into distribution
+prb = ProbabilityDistribution(data, 1./discretize)
 prb.plot(ax[0,0])
 
-dim = 2
-discretize=4
-x0s = np.linspace(0,1,discretize)
-x1s = np.linspace(0,1,discretize)
-x0, x1 = np.meshgrid(x0s, x1s)
-
-# get actions
-mesh = np.c_[x0.ravel(), x1.ravel()]
-# print(mesh)
-actions = range(len(mesh))
-
-# get utilities
-# print('generating utilities')
-
-# utils = [ 10 for a in actions]
-
-# utils = [mesh[a][0] for a in actions]
-utils = [0.1 for a in actions]
+# prepare inputs for the game
+util = UtilityFunctions.utility1
 
 
-# print('utils')
-# print(type(utils))
-# print(utils)
-# get distributions
-# for a in actions:
-#     print(prb.prb(a)[0])
-dist = [prb.prb(mesh[a])[0] for a in actions]
-# print('actions')
-# print(type(actions))
-# print(actions)
-# print('dist')
-# print(type(dist))
-# print(dist)
+def prepare_action_and_mesh(prb, discretize):
+    spaces = np.linspace(0, 1, discretize)
+    x = np.meshgrid(*[spaces] * prb.prb.d)
+    mesh = np.column_stack((xsub.ravel() for xsub in x))
+    actions = range(int(discretize ** prb.prb.d))
+    return actions, mesh, x
 
 
-g = Game(actions, utils, dist, 0.1, 0)
+actions, mesh, x = prepare_action_and_mesh(prb, discretize)
+
+g = Game(actions, util, mesh, prb.getPrb, FPrate, discount)
 g.solve()
-decisions = np.array(g.thetas).reshape(x0.shape)
-# print(decisions)
+decisions = np.array(g.thetas).reshape(x[0].shape)
 
-# cax = ax[1].imshow(decisions, extent=[0, 1, 0, 1])
+
+# Defender strategy
+ax[0,1].set_title('Defender probabilities')
+ax[0,1].set_xlabel(data.features[0])
+ax[0,1].set_ylabel(data.features[1])
 cax = ax[0,1].imshow(decisions, cmap=plt.get_cmap('Greys'), extent=[0, 1, 0, 1], vmin=0, vmax=1, interpolation='nearest')
 ax[0,1].set_xlim([0, 1])
 ax[0,1].set_ylim([0, 1])
 
-# attacker utilities
-# print(type(utils[0]))
-# print(type(g.thetas[0]))
-attack_utils = np.array([a*(1-b) for a,b in zip(utils, g.thetas)]).reshape(x0.shape)
-# print(attack_utils)
 
+#  Defender false positive
+ax[0,2].set_title('Real false-positives')
+ax[0,2].set_xlabel(data.features[0])
+ax[0,2].set_ylabel(data.features[1])
+cax = ax[0,2].imshow(np.array([prb.getPrb(mesh[a])*g.thetas[a] for a in actions]).reshape(x[0].shape), cmap=plt.get_cmap('Greys'), extent=[0, 1, 0, 1])
+ax[0,2].set_xlim([0, 1])
+ax[0,2].set_ylim([0, 1])
+
+# Attacker utilities
+ax[1,1].set_title('Attacker detected utilities')
+ax[1,1].set_xlabel(data.features[0])
+ax[1,1].set_ylabel(data.features[1])
+
+attack_utils = np.array([util(mesh[a])*(1-p) for a,p in zip(actions, g.thetas)]).reshape(x[0].shape)
 cax = ax[1,1].imshow(attack_utils, cmap=plt.cm.gist_earth_r, extent=[0, 1, 0, 1], vmin=0, vmax=1, interpolation='nearest')
 ax[1,1].set_xlim([0, 1])
 ax[1,1].set_ylim([0, 1])
 
 
 # original utilities
-cax = ax[1,0].imshow(np.array(utils).reshape(x0.shape), cmap=plt.cm.gist_earth_r, extent=[0, 1, 0, 1], vmin=0, vmax=1, interpolation='nearest')
+ax[1,0].set_title('Attacker undetected utilities')
+ax[1,0].set_xlabel(data.features[0])
+ax[1,0].set_ylabel(data.features[1])
+cax = ax[1,0].imshow(np.array([util(mesh[a]) for a in actions]).reshape(x[0].shape), cmap=plt.cm.gist_earth_r, extent=[0, 1, 0, 1], vmin=0, vmax=1, interpolation='nearest')
 ax[1,0].set_xlim([0, 1])
 ax[1,0].set_ylim([0, 1])
 
+
 plt.show()
-
-# X = np.mgrid[0:1:5j]
-# pos = np.vstack([X.ravel(), X.ravel()])
-# pos = np.meshgrid(X, X)
-# print(X)
-# print(pos)
-
 
